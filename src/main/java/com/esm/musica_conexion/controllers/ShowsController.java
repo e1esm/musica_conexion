6f7e391db3823c360947eed6d81d37f9381a2cdd
Egor Mikhailov <egor11022004@gmail.com>
2023-08-17 21:28:05 +0300

    Config for db

--- src/main/java/com/esm/musica_conexion/controllers/ShowsController.java
+++ src/main/java/com/esm/musica_conexion/controllers/ShowsController.java
@@ -1,13 +1,12 @@
 package com.esm.musica_conexion.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/v1")
 public class ShowsController {
-
-    @GetMapping(name = "/shows")
-    public String sayHello(){
-        return "Hey there broski!";
+    @GetMapping(name = "/shows", produces = "application/json")
+    public @ResponseBody String sayHello(){
+        return "Hey there!";
     }
 }
